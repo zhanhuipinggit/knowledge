64fbf59dc3233c97c0dc52ab80b2797dd0794dc3
zhanhuipinggit <18702529695@163.com>
2025-01-04 16:27:31 +0800

    two pointer array

--- algorithm/deduplication/code/common/listNode.go
+++ algorithm/deduplication/code/common/listNode.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 type ListNode struct {
 	Val  int
 	Next *ListNode
@@ -168,55 +166,5 @@ func detectCycle(head *ListNode) *ListNode {
 		fast = fast.Next
 	}
 	return slow
-
-}
-
-func main() {
-	l1 := &ListNode{1, nil}
-	l2 := &ListNode{10, nil}
-	l3 := &ListNode{8, nil}
-	l4 := &ListNode{8, nil}
-	l5 := &ListNode{20, nil}
-	l1.Next = l2
-	l2.Next = l3
-	l3.Next = l4
-	l4.Next = l5
-
-	r1 := &ListNode{3, nil}
-	r2 := &ListNode{2, nil}
-	r3 := &ListNode{0, nil}
-	r4 := &ListNode{-4, nil}
-	r1.Next = r2
-	r2.Next = r3
-	r3.Next = r4
-	r4.Next = r2
-
-	//s := mergeTwoList(l1, r1)
-	//for s != nil {
-	//	fmt.Println(s.Val)
-	//	s = s.Next
-	//}
-
-	//delListNodeUnique(l1)
-	//for l1 != nil {
-	//	fmt.Println(l1.Val)
-	//	l1 = l1.Next
-	//}
-
-	//s3 := partition(l1, 8)
-	//for s3 != nil {
-	//	fmt.Println(s3.Val)
-	//	s3 = s3.Next
-	//}
-
-	//s3 := detectCycle(r1)
-	//fmt.Println(s3.Val)
-	//re := &ListNode{1, nil}
-	s4 := removeNthFromEnd(l1, 1)
-	for s4 != nil {
-		fmt.Println(s4.Val)
-		s4 = s4.Next
-
-	}
 
 }
---
+++ algorithm/deduplication/code/common/twoPointerArray.go
@@ -0,0 +1,114 @@
+package main
+
+/**
+给你一个 非严格递增排列 的数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。元素的 相对顺序 应该保持 一致 。然后返回 nums 中唯一元素的个数。
+
+考虑 nums 的唯一元素的数量为 k ，你需要做以下事情确保你的题解可以被通过：
+
+更改数组 nums ，使 nums 的前 k 个元素包含唯一元素，并按照它们最初在 nums 中出现的顺序排列。nums 的其余元素与 nums 的大小不重要。
+返回 k 。
+*/
+// removeDuplicates
+func removeDuplicates(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	slow, fast := 0, 0
+	for fast < len(nums) {
+		if nums[slow] != nums[fast] {
+			slow++
+			nums[slow] = nums[fast]
+		}
+		fast++
+	}
+	return slow + 1
+
+}
+
+func RemoveDuplicatesList(head *ListNode) {
+	if head == nil || head.Next == nil {
+		return
+	}
+
+	slow, fast := head, head
+	for fast != nil {
+		if fast.Val != slow.Val {
+			slow.Next = fast
+			slow = slow.Next
+		}
+		fast = fast.Next
+	}
+
+}
+
+/**
+给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素。元素的顺序可能发生改变。然后返回 nums 中与 val 不同的元素的数量。
+*/
+
+func removeElement(nums []int, val int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	slow, fast := 0, 0
+	for fast < len(nums) {
+		if nums[fast] != val {
+			nums[slow] = nums[fast]
+			slow++
+		}
+		fast++
+	}
+	return slow
+
+}
+
+func moveZeroes(nums []int) {
+	p := removeElement(nums, 0)
+	for ; p < len(nums); p++ {
+		nums[p] = 0
+	}
+}
+
+/*
+*
+二分查找、左右指针
+*/
+func binarySearch(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left < right {
+		mid := (left + right) / 2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] > target {
+			right = mid - 1
+		} else if nums[mid] < target {
+			left = mid + 1
+		}
+	}
+	return -1
+}
+
+/**
+给你一个下标从 1 开始的整数数组 numbers ，该数组已按 非递减顺序排列  ，请你从数组中找出满足相加之和等于目标数 target 的两个数。如果设这两个数分别是 numbers[index1] 和 numbers[index2] ，则 1 <= index1 < index2 <= numbers.length 。
+
+以长度为 2 的整数数组 [index1, index2] 的形式返回这两个整数的下标 index1 和 index2。
+
+你可以假设每个输入 只对应唯一的答案 ，而且你 不可以 重复使用相同的元素。
+
+你所设计的解决方案必须只使用常量级的额外空间。
+*/
+
+func twoSum(nums []int, target int) []int {
+	left, right := 1, len(nums)
+	for left < right {
+		sum := nums[left] + nums[right]
+		if sum == target {
+			return []int{left, right}
+		} else if sum < target {
+			left++
+		} else if sum > target {
+			right--
+		}
+	}
+	return []int{-1, -1}
+}
